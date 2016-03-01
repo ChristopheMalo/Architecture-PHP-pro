32244008bcc2af87ce4f63b058eed97492fe4f86
ChristopheMalo <christof.malo@gmail.com>
2016-03-01 20:45:29 +0100

    feat: coder sql table commentaire

--- Database-SQL/script_sql/structure.sql
+++ Database-SQL/script_sql/structure.sql
@@ -11,10 +11,19 @@
 -- --------------------------------------------
 -- CREATION DE LA STRUCTURE DE LA TABLE article
 -- --------------------------------------------
+DROP TABLE if exists t_comment;
 DROP TABLE if exists t_article;
 
 CREATE TABLE t_article (
-    art_id INTEGER NOT NULL PRIMARY KEY auto-increment,
+    art_id INTEGER NOT NULL PRIMARY KEY auto_increment,
     art_title VARCHAR(100) NOT NULL,
     art_content VARCHAR(1000) NOT NULL
-) ENGINE=INNODB CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+) engine=innodb CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+CREATE TABLE t_comment (
+    com_id INTEGER NOT NULL PRIMARY KEY auto_increment,
+    com_author VARCHAR(100) NOT NULL,
+    comm_content VARCHAR(500) NOT NULL,
+    art_id INTEGER NOT NULL,
+    CONSTRAINT fk_com_art FOREIGN KEY(art_id) REFERENCES t_article(art_id)
+) engine=innodb CHARACTER SET utf8 COLLATE utf8_unicode_ci;
