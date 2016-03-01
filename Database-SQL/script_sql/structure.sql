7e491931f08b15b7bea558c44436268578c641c0
ChristopheMalo <christof.malo@gmail.com>
2016-03-01 20:41:56 +0100

    fix: corriger auto_increment structure sql

--- Database-SQL/script_sql/structure.sql
+++ Database-SQL/script_sql/structure.sql
@@ -14,7 +14,7 @@
 DROP TABLE if exists t_article;
 
 CREATE TABLE t_article (
-    art_id INTEGER NOT NULL PRIMARY KEY auto-increment,
+    art_id INTEGER NOT NULL PRIMARY KEY auto_increment,
     art_title VARCHAR(100) NOT NULL,
     art_content VARCHAR(1000) NOT NULL
 ) ENGINE=INNODB CHARACTER SET utf8 COLLATE utf8_unicode_ci;
