be72eed96bc44c60cbec7686e04281a7cf8ac6a6
ChristopheMalo <christof.malo@gmail.com>
2016-03-02 13:44:14 +0100

    feat: integrer twig bridge dans app

--- app/app.php
+++ app/app.php
@@ -11,29 +11,67 @@ use Symfony\Component\Debug\ExceptionHandler;
  * 
  * @author      Christophe Malo
  * @date        29/02/2016
- * @update      01/03/2016
- * @version     1.0.0
+ * @update      02/03/2016
+ * @version     1.0.2
  * @copyright   OpenClassrooms - Baptiste Pesquet
  * 
- * @commentaire update du 01/03/2016 : intégrer moteur template Twig au projet
+ * @commentaire v1.0.1 du 01/03/2016 : intégrer moteur template Twig au projet
+ *              v1.0.2 du 02/03/2016 : enregistrer le service d'accès aux commentaires
  */
 
-// Configuration de Silex pour gérer les potentielles erreurs
-// pendant l'execution de l'application
+/**
+ * Configuration de Silex pour gérer les potentielles erreurs
+ * pendant l'execution de l'application
+ */
 ErrorHandler::register();
 ExceptionHandler::register();
 
-// Enregistre le fournisseur de services associé à DBAL -> DoctrineServiceProvider
+
+/**
+ * Enregistrement des fournisseurs de services
+ * 
+ * 
+ */
+
+/**
+ * Enregistre le fournisseur de services associé à DBAL -> DoctrineServiceProvider
+ */
 $app->register(new Silex\Provider\DoctrineServiceProvider());
 
-// Enregistre Twig vers le chemin du dossier de templates (views) 
+/**
+ * Enregistre Twig vers le chemin du dossier de templates (views) 
+ */
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__ . '/../views',
 ));
 
-// Enregistre un service (dao.article) sous forme
-// d'une instance partagée de la classe ArticleDAO
+/**
+ * Enregistre le fournisseur de service Url associé au composant twig-bridge
+ */
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+
+
+
+/**
+ * Enregistrement des services
+ * 
+ * 
+ */
+
+/**
+ * Enregistre un service (dao.article) sous forme
+ * d'une instance partagée de la classe ArticleDAO
+ */
 $app['dao.article'] = $app->share(function ($app)
 {
     return new MicroCMS\DAO\ArticleDAO($app['db']);
+});
+
+/**
+ * Enregistre le service d'accès aux commentaires
+ */
+$app['dao.comment'] = $app->share(function ($app) {
+    $commentDAO = new MicroCMS\DAO\CommentDAO($app['db']);
+    $commentDAO->setArticleDAO($app['dao.Article']);
+    return $commentDAO;
 });
