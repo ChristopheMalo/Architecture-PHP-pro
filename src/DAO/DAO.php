b30f4d109dd8728d1b309ab7718ff604203f235c
ChristopheMalo <christof.malo@gmail.com>
2016-03-02 09:44:20 +0100

    refactor: coder la class DAO

---
+++ src/DAO/DAO.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace MicroCMS\DAO;
+
+use Doctrine\DBAL\Connection;
+
+/**
+ * MicroCMS
+ * =========================================================================================================
+ * 
+ * Data Access Object - Objet d'accès aux données
+ * 
+ * 
+ * @author      Christophe Malo
+ * @date        02/03/2016
+ * @version     1.0.0
+ * @copyright   OpenClassrooms - Victor Thuillier
+ */
+abstract class DAO
+{
+    
+    /**
+     * Connexion à la base de données - Objet
+     * 
+     * @var \Doctrine\DBAL\Connection
+     */
+    private $db;
+    
+    /**
+     * Le constucteur
+     * 
+     * @param object \Doctrine\DBAL\Connection L'objet de connexion à la DB
+     * @return void
+     */
+    public function __construct(Connection $db)
+    {
+        $this->db = $db;
+    }
+    
+    /**
+     * Donne accès à l'objet de connexion ) la DB
+     * 
+     * @return object \Doctrine\DBAL\Connection L'objet de connexion à la DB
+     */
+    protected function getDb()
+    {
+        return $this->db;
+    }
+    
+    /**
+     * Construit un objet de domaine (Article, Commentaire...) à partir d'une entrée de la DB
+     * Doit être définie par des classes enfants.
+     */
+    protected abstract function buildDomainObject($row);
+}
