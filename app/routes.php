b8698d146d3ae3a7d59e6ea4d9c0d6314357f14d
ChristopheMalo <christof.malo@gmail.com>
2016-03-04 18:38:19 +0100

    feat: coder la route pour le login form

--- app/routes.php
+++ app/routes.php
@@ -44,10 +44,18 @@ $app->get('/', function() use ($app) {
     return $app['twig']->render('index.html.twig', array('articles' => $articles));
 })->bind('home');
 
-// Artcile détaillé avec les commentaires
+// Article détaillé avec les commentaires
 $app->get('/article/{id}', function ($id) use ($app) {
     $article = $app['dao.article']->find($id);
     $comments = $app['dao.comment']->findAllByArticle($id);
     
     return $app['twig']->render('article.html.twig', array('article' => $article, 'comments' => $comments));
 })->bind('article');
+
+// Formulaire de login
+$app->get('/login', function(Request $request) use ($app) {
+    return $app['twig']->render('login.html.twig', array(
+        'error'         => $app['security.last_error']($request),
+        'last_username' => $app['session']->get('_security.last_username'),
+    ));
+})->bind('login');
