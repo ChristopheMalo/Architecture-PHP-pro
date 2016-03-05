af39a17c468e00833cc1fe8ccbb1530e571e6a1f
ChristopheMalo <christof.malo@gmail.com>
2016-03-05 14:39:12 +0100

    feat: updater route article pour comment

--- app/routes.php
+++ app/routes.php
@@ -1,6 +1,8 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
+use MicroCMS\Domain\Comment;
+use MicroCMS\Form\Type\CommentType;
 
 /**
  * MicroCMS
@@ -24,7 +26,8 @@ use Symfony\Component\HttpFoundation\Request;
  */
 
 // Page d'accueil -> route correspondant à l'URL racine de l'application ('/')
-$app->get('/', function() use ($app) {
+$app->get('/', function() use ($app)
+{
     
     // Utilisation du service dao.article enregistré dans app/app.php
     // Cet appel à $app['dao.article'] renvoie un objet de la class ArticleDAO
@@ -47,16 +50,49 @@ $app->get('/', function() use ($app) {
     return $app['twig']->render('index.html.twig', array('articles' => $articles));
 })->bind('home');
 
+
+
 // Article détaillé avec les commentaires
-$app->get('/article/{id}', function ($id) use ($app) {
+// match permet de gérer Get et Post
+$app->match('/article/{id}', function ($id, Request $request) use ($app)
+{
+    
     $article = $app['dao.article']->find($id);
+    $commentFormView = null;
+    
+    if ($app['security.authorization_checker']->isGranted('IS_AUTHENTICATED_FULLY'))
+    {
+        // L'utilisateur est authentifié, il peut commenter
+        $comment = new Comment();
+        $comment->setArticle($article);
+        $user = $app['user'];
+        $comment->setAuthor($user);
+        $commentForm = $app['form.factory']->create(new CommentType(), $comment); // Création du commentaire
+        $commentForm->handleRequest($request); // Soumission du formulaire
+        
+        if ($commentForm->isSubmitted() && $commentForm->isValid())
+        {
+            $app['dao.comment']->save($comment);
+            $app['session']->getFlashBag()->add('success', 'Votre commentaire a été ajouté');
+        }
+        
+        $commentFormView = $commentForm->createView();
+    }
+    
     $comments = $app['dao.comment']->findAllByArticle($id);
     
-    return $app['twig']->render('article.html.twig', array('article' => $article, 'comments' => $comments));
+    return $app['twig']->render('article.html.twig', array(
+        'article'       => $article,
+        'comments'      => $comments,
+        'commentForm'   => $commentFormView));
+    
 })->bind('article');
 
+
+
 // Formulaire de login
-$app->get('/login', function(Request $request) use ($app) {
+$app->get('/login', function(Request $request) use ($app)
+{
     return $app['twig']->render('login.html.twig', array(
         'error'         => $app['security.last_error']($request),
         'last_username' => $app['session']->get('_security.last_username'),
